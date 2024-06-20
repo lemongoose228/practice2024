8faf39a1ccbf80ca48c005bfb56bf7582b39b38e
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-06-21 00:39:01 +0800

    Publishing data (unfinished)

--- parser.py
+++ parser.py
@@ -1,21 +1,53 @@
+import time
+import paho.mqtt.client as mqtt_client
 from gnss_tec import rnx
 
-import sys
+broker = "broker.emqx.io"
 
-filename = sys.argv[1]
 
-with open(filename) as obs_file:
+client = mqtt_client.Client(
+   mqtt_client.CallbackAPIVersion.VERSION1,
+   'isu1001230065455645543'
+)
+
+print("Connecting to broker", broker)
+print(client.connect(broker))
+client.loop_start()
+print("Publishing")
+
+with open('FLRS00PRT_S_20240010000_01D_30S_MO.rnx') as obs_file:
     reader = rnx(obs_file)
-    a = 0
     for tec in reader:
-        print(
-            '{} {}: {} {}'.format(
+        state = '{} {}: {} {}'.format(
                 tec.timestamp,
                 tec.satellite,
                 tec.phase_tec,
                 tec.p_range_tec,
             )
-        )
-        a += 1
-        if a == 5:
-            break
+    client.publish("lab/leds/state", state)
+
+
+client.disconnect()
+client.loop_stop()
+
+# from gnss_tec import rnx
+# 
+# import sys
+# 
+# filename = sys.argv[1]
+# 
+# with open(filename) as obs_file:
+#     reader = rnx(obs_file)
+#     a = 0
+#     for tec in reader:
+#         print(
+#             '{} {}: {} {}'.format(
+#                 tec.timestamp,
+#                 tec.satellite,
+#                 tec.phase_tec,
+#                 tec.p_range_tec,
+#             )
+#         )
+#         a += 1
+#         if a == 5:
+#             break
