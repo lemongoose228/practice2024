fa852bdeae56b16ce12fd1fcf5869002b059fe4f
Astrof123 <71438030+Astrof123@users.noreply.github.com>
2024-06-25 22:13:29 +0800

    Added so that the publisher switches to a new file at 00:00. And so that the newly created publishers wait for a new day.

--- publisher.py
+++ publisher.py
@@ -3,101 +3,124 @@ import paho.mqtt.client as mqtt_client
 import hashlib
 import datetime
 import sys
-# import systemd.daemon
+import systemd.daemon
 
 from gnss_tec import rnx
 
 def get_rnx_number():
-    with open('variable.txt', 'r') as f:
-       rnx_number = f.read().strip()
+    with open('/home/user/practice/variables.txt', 'r') as f:
+        rnx_number = ''
+        while 'rnx' not in rnx_number:
+            rnx_number = f.readline().strip()
 
     return rnx_number
 
-broker = "broker.emqx.io"
+def broker_setup():
+    broker = "broker.emqx.io"
 
-# filename = sys.argv[1]
-filename = "DRAO00CAN_R_20240070000_01D_30S_MO.rnx"
+    time_str = str(datetime.datetime.now())
+    user_id = hashlib.md5(time_str.encode()).hexdigest()
 
-print("info/" + filename[0:9])
+    client = mqtt_client.Client(
+        mqtt_client.CallbackAPIVersion.VERSION2,
+        user_id[0:12]
+    )
 
-time_str = str(datetime.datetime.now())
-user_id = hashlib.md5(time_str.encode()).hexdigest()
+    print("Connecting to broker", broker)
+    client.connect(broker)
+    client.loop()
+    print("Publishing")
 
-client = mqtt_client.Client(
-   mqtt_client.CallbackAPIVersion.VERSION2,
-   user_id[0:12]
-)
+    return client
 
-print("Connecting to broker", broker)
-client.connect(broker)
-client.loop()
-print("Publishing")
+def get_begin_work_time():
+    real_time = datetime.datetime.now()
+    # real_time = datetime.datetime(2024, 1, 1, 23, 59, 21)
 
-# systemd.daemon.notify('READY=1')
-
-
-fullpath = f"{get_rnx_number()}/{filename}"
-
-real_time = datetime.datetime.now()
-
-if 20 <= int(real_time.strftime("%H:%M:%S").split(":")[-1]) <= 50:
-    real_time += datetime.timedelta(minutes=1)
-    real_time = real_time.strftime("%H:%M:%S")
-
-    begin_work_hour = real_time.split(":")[0]
-    begin_work_minutes = real_time.split(":")[1]
-
-    begin_work_time = f"{begin_work_hour}:{begin_work_minutes}:00"
-else:
-    if int(real_time.strftime("%H:%M:%S").split(":")[-1]) >= 50:
+    if 20 <= int(real_time.strftime("%H:%M:%S").split(":")[-1]) <= 50:
         real_time += datetime.timedelta(minutes=1)
         real_time = real_time.strftime("%H:%M:%S")
+
         begin_work_hour = real_time.split(":")[0]
         begin_work_minutes = real_time.split(":")[1]
 
-        begin_work_time = f"{begin_work_hour}:{begin_work_minutes}:30"
-
+        begin_work_time = f"{begin_work_hour}:{begin_work_minutes}:00"
     else:
-        real_time = real_time.strftime("%H:%M:%S")
-        begin_work_hour = real_time.split(":")[0]
-        begin_work_minutes = real_time.split(":")[1]
+        if int(real_time.strftime("%H:%M:%S").split(":")[-1]) >= 50:
+            real_time += datetime.timedelta(minutes=1)
+            real_time = real_time.strftime("%H:%M:%S")
+            begin_work_hour = real_time.split(":")[0]
+            begin_work_minutes = real_time.split(":")[1]
 
-        begin_work_time = f"{begin_work_hour}:{begin_work_minutes}:30"
+            begin_work_time = f"{begin_work_hour}:{begin_work_minutes}:30"
 
-with open(fullpath) as obs_file:
-    reader = rnx(obs_file)
-    current_time = -1
-    start_time = 0
-    end_time = 30
-    ready_work = False
-
-    for tec in reader:
-        state = '{} {}: {} {}'.format(
-                tec.timestamp,
-                tec.satellite,
-                tec.phase_tec,
-                tec.p_range_tec,
-            )
-        if not(ready_work):
-            if state.split()[1] == begin_work_time:
-                while datetime.datetime.now().strftime("%H:%M:%S") != begin_work_time:
-                    time.sleep(0.1)
-                ready_work = True
-            else:
-                continue
-
-        if current_time == state.split()[1]:
-            client.publish("info/" + filename[0:9], state)
         else:
-            if current_time != -1:
-                end_time = time.time()
-                execution_time = end_time - start_time
-                time.sleep(30 - execution_time)
+            real_time = real_time.strftime("%H:%M:%S")
+            begin_work_hour = real_time.split(":")[0]
+            begin_work_minutes = real_time.split(":")[1]
 
-            current_time = state.split()[1]
+            begin_work_time = f"{begin_work_hour}:{begin_work_minutes}:30"
 
-            client.publish("info/" + filename[0:9], state)
-            start_time = time.time()
+    return begin_work_time
+
+def publishing(fullpath):
+    begin_work_time = get_begin_work_time()
+
+    with open(fullpath) as obs_file:
+        reader = rnx(obs_file)
+        parsing_current_time = -1
+        start_time = 0
+        ready_work = False
+
+        for tec in reader:
+            state = '{} {}: {} {}'.format(
+                    tec.timestamp,
+                    tec.satellite,
+                    tec.phase_tec,
+                    tec.p_range_tec,
+                )
+            if not(ready_work):
+                if state.split()[1] == begin_work_time:
+                    while datetime.datetime.now().strftime("%H:%M:%S") != begin_work_time:
+                        time.sleep(0.1)
+                    ready_work = True
+                else:
+
+                    continue
+
+            if parsing_current_time == state.split()[1]:
+                client.publish("info/" + filename[0:9], state)
+            else:
+                if parsing_current_time != -1:
+                    end_time = time.time()
+                    execution_time = end_time - start_time
+                    time.sleep(30 - execution_time)
+
+                parsing_current_time = state.split()[1]
+
+                client.publish("info/" + filename[0:9], state)
+                start_time = time.time()
 
 
-        print("message is " + state)
+            print("message is " + state)
+
+filename = sys.argv[1]
+status = sys.argv[2]
+# filename = "DRAO00CAN_R_20240070000_01D_30S_MO.rnx"
+# status = 'work'
+
+if status == 'wait':
+    while datetime.datetime.now().strftime("%H:%M:%S") != "23:59:21":
+        time.sleep(0.1)
+
+systemd.daemon.notify('READY=1')
+client = broker_setup()
+
+fullpath = f"/home/user/practice/{get_rnx_number()}/{filename}"
+
+print(fullpath)
+
+while True:
+    publishing(fullpath)
+    fullpath = f"/home/user/practice/{get_rnx_number()}/{filename}"
+
