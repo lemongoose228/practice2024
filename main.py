6a0768690da2859daccaa04ee2eea06ff790381a
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-06-27 00:12:22 +0800

    Handling data download interruption and resuming download

--- main.py
+++ main.py
@@ -5,8 +5,45 @@ import os
 
 app = FastAPI()
 
-#class getStreams(BaseModel):
-#    streams: List
+def downloading(date, check):
+    file_name = f"data/{date[0:4]}/{date[5:]}.zip"
+    link = f"https://api.simurg.space/datafiles/map_files?date={date}"
+
+    start_byte = 0
+    if os.path.exists(file_name):
+        start_byte = os.path.getsize(file_name)
+
+    headers = {"Range": f"bytes={start_byte}-"}
+
+    with open(file_name, "ab") as f:
+        print("Downloading %s" % file_name)
+        response = requests.get(link, headers=headers, stream=True)
+
+        if check:
+            if response.status_code == 200:
+                return True
+            else:
+                return False
+
+        total_length = int(response.headers.get('content-length', 0)) - start_byte
+        dl = start_byte
+
+        if total_length:
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / total_length)
+                sys.stdout.write("\r[%s%s] %3.1f%%" % ('=' * done, ' ' * (50 - done), 100 * dl / total_length))
+                sys.stdout.flush()
+
+        else:
+            f.write(response.content)
+
+    print("\nDownload completed for %s" % file_name)
+
+    return True
+
+
 
 class getListReceivers(BaseModel):
     date: str
